5a3dcced725aee59ea953e478c3354143697c06d
Glenn Gillen <me@glenngillen.com>
2020-01-28 17:55:45 +1100

    Remove AWS stuff.

--- aws.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  version = "~> 2.17"
-  region = "us-east-1"
-  assume_role {
-    role_arn     = "arn:aws:iam::985898799555:role/OrganizationAccountAccessRole"
-    session_name = "TerraformDemo"
-  }
-}
