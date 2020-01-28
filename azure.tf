3f6daa179d3f1b346bc7192102a5da5d13bfd410
Glenn Gillen <me@glenngillen.com>
2020-01-28 18:44:54 +1100

    Can't use random in variables.

--- azure.tf
+++ azure.tf
@@ -4,15 +4,9 @@ provider "azurerm" {
   version = "=1.34.0"
 }
 
-resource "random_password" "password" {
-  length = 16
-  special = true
-  override_special = "_%@"
-}
-
 variable "prefix" {
   default = "hashicorp-example"
 }
 variable "username" {
-  default = random_password.password
+  default = "ggillen"
 }
