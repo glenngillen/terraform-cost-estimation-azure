8a826935e5ef814e2c0217f700d278dad6ad90a3
Glenn Gillen <me@glenngillen.com>
2020-02-03 11:19:23 +1100

    Add autoscaler.

--- azure-vms.tf
+++ azure-vms.tf
@@ -2,14 +2,53 @@ variable "vm_size" {
   default = "Standard_B1s"
 }
 variable "capacity" {
-  default = 2
+  default = 0
 }
+
+resource "azurerm_autoscale_setting" "web-scaler" {
+  name                = "myAutoscaleSetting"
+  enabled             = true
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  target_resource_id  = azurerm_virtual_machine_scale_set.prod-web-servers.id
+
+  profile {
+    name = "forJuly"
+
+    capacity {
+      default = 0
+      minimum = 0
+      maximum = 10
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_virtual_machine_scale_set.prod-web-servers.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 90
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+}
+
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = "${azurerm_resource_group.main.location}"
-  resource_group_name   = "${azurerm_resource_group.main.name}"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
   network_interface_ids = ["${azurerm_network_interface.main.id}"]
-  vm_size               = "${var.vm_size}"
+  vm_size               = var.vm_size
 
   delete_os_disk_on_termination = true
   delete_data_disks_on_termination = true
@@ -28,33 +67,33 @@ resource "azurerm_virtual_machine" "main" {
   }
   os_profile {
     computer_name  = "hostname"
-    admin_username = "${var.username}"
-    admin_password = "${random_password.password.result}"
+    admin_username = var.username
+    admin_password = random_password.password.result
 
     custom_data = "echo 'init test'"
   }
   os_profile_linux_config {
     ssh_keys {
-      key_data = "${var.public_key}"
+      key_data = var.public_key
       path = "/home/${var.username}/.ssh/authorized_keys"
     }
     disable_password_authentication = true
   }
   tags = {
     environment = "test"
-    owner = "${var.username}"
+    owner = var.username
     organization = "hashicorp"
     application = "example"
   }
 }
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.internal.id}"
+    subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
@@ -66,27 +105,27 @@ resource "azurerm_resource_group" "main" {
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = "${azurerm_resource_group.main.name}"
-  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefix       = "10.0.2.0/24"
 }
 
 resource "azurerm_virtual_machine_scale_set" "prod-web-servers" {
   name                  = "${var.prefix}-vm"
-  location              = "${azurerm_resource_group.main.location}"
-  resource_group_name   = "${azurerm_resource_group.main.name}"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
   upgrade_policy_mode   = "Automatic"
 
   sku {
-    name     = "${var.vm_size}"
+    name     = var.vm_size
     tier     = "Standard"
-    capacity = "${var.capacity}"
+    capacity = var.capacity
   }
   network_profile {
     name    = "WebNetworkProfile"
@@ -94,7 +133,7 @@ resource "azurerm_virtual_machine_scale_set" "prod-web-servers" {
     ip_configuration {
       name      = "${var.prefix}-nic"
       primary   = true
-      subnet_id = "${azurerm_subnet.internal.id}"
+      subnet_id = azurerm_subnet.internal.id
     }
   }
   storage_profile_os_disk {
@@ -111,15 +150,15 @@ resource "azurerm_virtual_machine_scale_set" "prod-web-servers" {
   }
   os_profile {
     computer_name_prefix = "${var.prefix}-vm-"
-    admin_username = "${var.username}"
-    admin_password = "${random_password.password.result}"
+    admin_username = var.username
+    admin_password = random_password.password.result
 
     custom_data = "echo 'init test'"
   }
   os_profile_linux_config {
     disable_password_authentication = true
     ssh_keys {
-      key_data = "${var.public_key}"
+      key_data = var.public_key
       path = "/home/${var.username}/.ssh/authorized_keys"
     }
   }
